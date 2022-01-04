df0a4579bd53fee6aab71a03da2ed1a3b1c08d7a
Ígor Pedro Leitão Mascarenhas Martins <iplmmartins@ibm.com>
2022-01-04 00:31:02 -0300

    create my reduce

--- lists/for-forin/4.reduce.js
+++ lists/for-forin/4.reduce.js
@@ -1,14 +1,34 @@
 const { getPeople } = require('./services')
 
+Array.prototype.myReduce = function(callback, valueInitial) {
+    let valueFinal = typeof valueInitial !== undefined ? valueInitial : this[0]
+
+    for(let index = 0; index <= this.length -1; index++) {
+        valueFinal = callback(valueFinal, this[index], this)
+    }
+
+    return valueFinal
+}
+
 async function main() {
     try {
         const { results } = await getPeople('a')
 
         const height = results.map(item => parseInt(item.height))
 
-        const total = height.reduce((previous, next) => {
+        /* const total = height.reduce((previous, next) => {
             return previous + next
-        })
+        }) */
+
+        const myList = [
+            ['Igor', 'Pedro'],
+            ['Node.js', 'NodeBR']
+        ]
+
+        const total = myList.myReduce((previous, next) => {
+            return previous.concat(next)
+        }, [])
+        .join(', ')
 
         console.log(total)
     } catch (error) {
